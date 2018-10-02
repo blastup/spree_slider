d46e9e64b44e4755836aea99c669b35dbfee7ae1
Nicolas Roumeliotis <nicroumeliotis91@gmail.com>
2018-10-02 18:13:38 +0300

    temporary filter of slide locations until pages are refactored

--- app/controllers/spree/api/v1/slide_locations_controller.rb
+++ app/controllers/spree/api/v1/slide_locations_controller.rb
@@ -4,7 +4,11 @@ module Spree
       class SlideLocationsController < Spree::Api::BaseController
 
         def index
-          @slide_locations = Spree::SlideLocation.order(:name)
+          # Blastup - Partial Refactor
+          # Respond only with the slide_locations that are selected.
+          keys_to_select = ["product_list_top_slide_location_id", "product_page_top_slide_location_id", "login_slider_location_id"]
+          selected_slide_locations = Spree::ExtraSetting.where(key: keys_to_select).pluck(:value).uniq
+          @slide_locations = Spree::SlideLocation.where(id: selected_slide_locations).order(:name)
           respond_with(@slide_locations)
         end
       end
