219bcca26644b73281beccd456c63f5b42d537a9
Ap Theod <aptheod.cs@gmail.com>
2018-10-22 12:56:10 +0300

    Fetch slide locations by id

--- app/controllers/spree/api/v1/slide_locations_controller.rb
+++ app/controllers/spree/api/v1/slide_locations_controller.rb
@@ -4,11 +4,15 @@ module Spree
       class SlideLocationsController < Spree::Api::BaseController
 
         def index
-          # Blastup - Partial Refactor
-          # Respond only with the slide_locations that are selected.
-          keys_to_select = ["product_list_top_slide_location_id", "product_page_top_slide_location_id", "login_slider_location_id"]
-          selected_slide_locations = Spree::ExtraSetting.where(key: keys_to_select).pluck(:value).uniq
-          @slide_locations = Spree::SlideLocation.where(id: selected_slide_locations).order(:name)
+          if params[:id] && !params[:id].blank?
+            @slide_locations = Spree::SlideLocation.where(id: params[:id])
+          else
+            # Blastup - Partial Refactor
+            # Respond only with the slide_locations that are selected.
+            keys_to_select = ["product_list_top_slide_location_id", "product_page_top_slide_location_id", "login_slider_location_id"]
+            selected_slide_locations = Spree::ExtraSetting.where(key: keys_to_select).pluck(:value).uniq
+            @slide_locations = Spree::SlideLocation.where(id: selected_slide_locations).order(:name)
+          end
           respond_with(@slide_locations)
         end
       end
