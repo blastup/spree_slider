5762f21595abf003bffcebb800d685e0b6f88d88
Nicolas Roumeliotis <nicroumeliotis91@gmail.com>
2018-11-09 12:22:41 +0200

    api return all slide locations

--- app/controllers/spree/api/v1/slide_locations_controller.rb
+++ app/controllers/spree/api/v1/slide_locations_controller.rb
@@ -5,13 +5,13 @@ module Spree
 
         def index
           if params[:id] && !params[:id].blank?
-            @slide_locations = Spree::SlideLocation.where(id: params[:id])
+            @slide_locations = Spree::SlideLocation.active.where(id: params[:id])
           else
             # Blastup - Partial Refactor
             # Respond only with the slide_locations that are selected.
             keys_to_select = ["product_list_top_slide_location_id", "product_page_top_slide_location_id", "login_slider_location_id", "top_footer_area_id"]
             selected_slide_locations = Spree::ExtraSetting.where(key: keys_to_select).pluck(:value).uniq
-            @slide_locations = Spree::SlideLocation.where(id: selected_slide_locations).order(:name)
+            @slide_locations = Spree::SlideLocation.active.where(id: selected_slide_locations).order(:name)
           end
           respond_with(@slide_locations)
         end
